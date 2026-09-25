f12c74038e6410162002a867977d135d92f22e68
Manav <manavkhugshalwork@gmail.com>
2026-09-25 22:57:38 +0530

    Program to find Greatest common factor

---
+++ 030-Greatest_common_factor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n;
+    cout << "Enter two numbers : ";
+    cin >> m >> n;
+    while (m != n)
+    {
+        if (m > n)
+        {
+            m = m - n;
+        }
+        else if (n > m)
+        {
+            n = n - m;
+        }
+    }
+    cout << "The greatest common factor is " << m;
+    return 0;
+}
