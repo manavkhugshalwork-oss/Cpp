ef63faf166324d8d7f9b113869b10b26fbf584c1
Manav <manavkhugshalwork@gmail.com>
2026-09-25 22:57:01 +0530

    Program to check for Armstrong number

---
+++ 028-Armstrong_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n, r, sum = 0;
+    cout << "Enter a number : ";
+    cin >> n;
+    m = n;
+    while (n > 0)
+    {
+        r = n % 10;
+        n = n / 10;
+        sum = sum + r * r * r;  
+    }
+    if (sum == m)
+    {
+        cout << m << " is an armstrong number";
+    }
+    else
+    {
+        cout << m << " is not an armstrong number";
+    }
+    return 0;
+}
